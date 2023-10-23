910c89d54b80e7d7f99587ea497355188a0c1cb8
mariana <marianass712@gmail.com>
2023-10-23 20:08:00 -0300

    se realizo modificaciones en el index

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -2,8 +2,8 @@
 Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)*/
 let frase = prompt("Introduce una frase:");
 let vocales = "";
-for (var i = 0; i < frase.length; i++) {
-  var caracter = frase.charAt(i);
+for (let i = 0; i < frase.length; i++) {
+  let caracter = frase.charAt(i);
   if ("aeiou".indexOf(caracter) !== -1) {
     if (vocales.indexOf(caracter) === -1) {
       vocales += caracter;
